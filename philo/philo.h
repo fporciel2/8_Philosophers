bcd1cf254c9d0aa224d71d8ec8a78df8ebde2e15
fporciel <fporciel@student.42roma.it>
2023-12-08 11:11:49 +0100

    Why swap?

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:05:21 by fporciel          #+#    #+#             */
-/*   Updated: 2023/12/08 09:24:15 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/12/08 10:47:57 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -38,5 +38,7 @@
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+
 
 #endif
