bdba0ac8299d0fce533ba42a9314590a8f6c1a68
fporciel <fporciel@student.42roma.it>
2023-12-31 10:54:18 +0100

    Restart.

---
+++ philo/philo.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/31 10:53:52 by fporciel          #+#    #+#             */
+/*   Updated: 2023/12/31 10:54:00 by fporciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+ * 'Philosophers' is a simulation program that explores concurrent programming
+ * with threads, mutexes and semaphores. It is an implementation of a solution
+ * of a variation on the dining philosophers problem by Edsger Dijkstra.
+ *
+ * Copyright (C) 2023  Federico Porciello
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
